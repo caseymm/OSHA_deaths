87928ab0d8153a30a5259b32403cd146ac5ee3bf
caseymm <caseymm@gmail.com>
2014-04-21 09:56:05 -0400

    finished geocodes & added color vars to css

--- scripts/extract_older_dates.py
+++ scripts/extract_older_dates.py
@@ -3,7 +3,7 @@ import csv
 import re
 
 f=open(raw_input('Output file: '),'w')
-with open('../data/original/FatalitiesFY10.csv', 'rU') as csvfile:
+with open('../data/original/FatalitiesFY09.csv', 'rU') as csvfile:
     fp = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in fp:
         date = row[2]
